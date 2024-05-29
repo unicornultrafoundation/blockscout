aa79a9afbd8eda8f4e4f12b21a3f3f433675a58e
ngdlong91 <ngdlong91@gmail.com>
2024-05-30 03:48:37 +0700

    Cannot fetch url

--- apps/block_scout_web/lib/block_scout_web/controllers/api/v2/token_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/api/v2/token_controller.ex
@@ -33,7 +33,7 @@ defmodule BlockScoutWeb.API.V2.TokenController do
       TokenTotalSupplyOnDemand.trigger_fetch(address_hash)
       Logger.info "API token"
       Logger.info "Token #{inspect(token)}"
-      base_icon_url = "https://raw.githubusercontent.com/unicornultrafoundation/token-assets/master/tokens/#{hash}/logo.png"
+      base_icon_url = "https://raw.githubusercontent.com/unicornultrafoundation/token-assets/master/tokens/#{address_hash}/logo.png"
       %HTTPoison.Response{status_code: status_code} = HTTPoison.get!(base_icon_url)
       if status_code == 200 do
         token = Map.put(token, :icon_url, base_icon_url)
